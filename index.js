111378345358e02cd6182ce6fba6158e1fa39e01
Mavlyan <dungan_km@mail.ru>
2023-12-06 10:27:36 +0600

    test

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ const uri = process.env.MONGODB_URI;
 
 mongoose.connect(uri, { 
     useNewUrlParser: true, 
-    useUnifiedTopology: true 
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000
 })
     .then(() => {
         console.log('Connected to MongoDB');
