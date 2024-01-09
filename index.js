f6369accecf732c6095036f0f3efe65e6330a28c
Mavlyan <dungan_km@mail.ru>
2024-01-09 21:14:10 +0600

    telegram

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5151;
 const botToken = process.env.botToken
-const chatId = process.env.chatId
+// const chatId = process.env.chatId
+const chatId = '-4011710662'
 const url = process.env.MONGODB_URI
-const telegramUrl = `https://api.telegram.org/bot${botToken}/sendMessage`
+const telegramUrl = `https://api.telegram.org/bot6919234850:AAGheulm8qoO-8yHp7Bs4c51Pp7DAC3KGVw/sendMessage`
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(express.json({ limit: '50mb' }));
